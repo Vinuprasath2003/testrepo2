b2be73bcbb1a092822e1718ff75ac1e72273cd02
Vinuprasath2003 <129865984+Vinuprasath2003@users.noreply.github.com>
2024-01-08 15:23:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,1 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "5.10.0"
-    }
-  }
-}
 
-provider "google" {
-    project ="affable-audio-410209"
-    region = "us-central1"
-    zone ="us-central1-a"
-    credentials ="./cred.json"
-}
